6ac080469ca68d3c140b119ee52ee1b2e55477d7
Mehdi Raza <mehdics100@gmail.com>
2025-06-11 11:45:44 +0500

    Backend integrated with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const dbConnect = require("./mongoose/mongooseConnect");
+const Contact = require("./models/Contact");
 
 dotenv.config();
 const app = express();
@@ -9,15 +11,20 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json()); // To parse JSON requests
+dbConnect();
 
 // Contact form route
-app.post("/api/contact", (req, res) => {
+app.post("/api/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
-  // Later: Save to MongoDB
-  console.log("Contact Form Submission:", { name, email, message });
-
-  res.status(200).json({ message: "Form submitted successfully!" });
+  try {
+    const newContact = new Contact({ name, email, message });
+    await newContact.save();
+    console.log("Contact Form Submission:", { name, email, message });
+    res.status(200).json({ message: "Form submitted" });
+  } catch (error) {
+    console.log("Error Encountered while storing a message: ", error.message);
+  }
 });
 
 // Start server
